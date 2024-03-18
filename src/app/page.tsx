44a121bf478f32e4d54ea593ac77258dda4c1a07
Rafiad <rafiadhoseen@gmail.com>
2024-03-18 15:38:47 +0600

    Add All file

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import ExpenseForm from "@/components/ExpenseForm";
+import ExpenseList from "@/components/ExpenseList";
+import React, { useState } from "react";
+
+export type Expense = {
+  id: number;
+  description: string;
+  amount: number;
+};
+
+export default function Home() {
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+
+  const addExpense = (expense: Expense) => {
+    setExpenses((prevExpenses) => [...prevExpenses, expense]);
+  };
+
+  const deleteExpense = (id: number) => {
+    setExpenses((prevExpenses) =>
+      prevExpenses.filter((expense: Expense) => expense.id !== id)
+    );
+  };
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4 text-center pb-4 text-gray-500">
+        EXPENSE TRACKER
+      </h1>
+      <ExpenseForm onAddExpense={addExpense} />
+      {expenses.length ? (
+        <ExpenseList expenses={expenses} onDeleteExpense={deleteExpense} />
+      ) : null}
+    </div>
+  );
+}
